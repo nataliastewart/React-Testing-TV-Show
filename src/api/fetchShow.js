ca4e4e659492154cdf2c21d5ed07513de09db936
nataliastewart <nataliastewart@users.noreply.github.com>
2020-04-20 18:40:09 -0400

    moving async call to another directory

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,13 +1,12 @@
 import { axios } from "axios";
 
-export default function fetchShow() {
-  axios
+export const fetchShow = () => {
+  return axios
     .get(
       "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
     )
     .then((res) => {
-      console.log("APP-RES.DATA", res.data);
-      setShow(res.data);
-      setSeasons(formatSeasons(res.data._embedded.episodes));
+      return res.data;
+      // console.log("APP-RES", res);
     });
-}
+};
